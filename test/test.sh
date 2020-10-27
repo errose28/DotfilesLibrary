4f815471809bfd068ae1c7b94cb939347c59f58b
Ethan Rose <errose28@gmail.com>
2020-10-26 21:46:28 -0400

    Add comments to test launcher

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 if [ "$1" = '-i' ]; then
+    # Run tests and place user interactively in container to inspect the results.
     docker-compose run --rm test_link /bin/sh
 else
+    # Run tests and display the results.
     docker-compose run --rm test_link
 fi
 
