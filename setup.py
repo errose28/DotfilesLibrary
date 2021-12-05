ac34ead57269255ab883a3d6d532a451ed7e977a
Ethan Rose <erose@apache.org>
2021-12-05 12:31:06 -0800

    Set master version to 0.2.0-alpha

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name = 'DotfilesLibrary',
-  version = '0.1.0',
+  version = '0.2.0-alpha',
   author = 'Ethan Rose',
   author_email = 'errose28@gmail.com',
   packages = setuptools.find_packages(),
